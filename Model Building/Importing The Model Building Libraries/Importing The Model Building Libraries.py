ce9c6db6962b6dbbd83e6c5c94c89336a4a6585e
BGJGA <94361468+BGJGA@users.noreply.github.com>
2022-11-18 15:34:55 +0530

    Add files via upload

---
+++ Model Building/Importing The Model Building Libraries/Importing The Model Building Libraries.py
@@ -0,0 +1,12 @@
+'import model building libraries'
+#To define linear intialisation import Sequential from keras.models import Sequential
+#To add Layers import Dense
+from keras.layers import Dense
+#To create Convolution kernel import convolution2D
+from keras.layers import Convolution2D
+#import Maxpooling layer
+from keras.layers import MaxPooling2D
+#import Flatten Layer
+from keras.layers import Flatten
+import warnings
+warnings.filterwarnings('ignore')
